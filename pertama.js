fa680017c992e7ed99dd69d2f3f79e83b29ae929
Nashi113 <ikuameru12@gmail.com>
2025-09-22 21:31:23 +0700

    Tugas 3

---
+++ pertama.js
@@ -0,0 +1,9 @@
+let buah = ["anggur", "rambutan", "alpukat"]; //deklarasi sebuah array
+buah[2] = "Markisa"; // pengubahan index
+let barang = new Array("Motor", "Monitor", "Mobil"); //dekralasi menggunakan new array
+let barangKosong = new Array(5); // panjang data, bukan value atau datanya
+
+console.log(buah[2]);
+console.log(barang);
+console.log(barangKosong);
+console.log(buah.construktor);
